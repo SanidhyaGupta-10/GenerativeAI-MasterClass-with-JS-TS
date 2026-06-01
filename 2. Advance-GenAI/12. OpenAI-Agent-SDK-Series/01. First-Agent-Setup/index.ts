f30f6efeefc4ce336808f30b978cac299546cbb0
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-06-01 20:04:27 +0530

    feat: implement initial OpenAI Agent setup using Ollama provider

--- 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/01. First-Agent-Setup/index.ts
+++ 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/01. First-Agent-Setup/index.ts
@@ -1,23 +1,34 @@
 // console.log("Setting-up Our First Agent with OpenAI-SDK in TypeScript");
-
-import OpenAI from "openai"
 import "dotenv/config";
 import {
-    OpenAIChatCompletionsModel, run, Agent
-} from '@openai/agents'
+    OpenAIChatCompletionsModel, run, Agent, setTracingDisabled
+} from '@openai/agents';
+import OpenAI from "openai";
 
+// This is will need if you are using ollama because 
+setTracingDisabled(true)
 
-// 1. Initialize OpenAI client to point to your local Ollama instance
 const ollamaClient = new OpenAI({
     baseURL: 'http://localhost:11434/v1/',
     apiKey: 'ollama', // Required by the SDK but ignored by Ollama
     dangerouslyAllowBrowser: true
 });
 
-
 // 2. Configure the model wrapper with the custom client
 const ollamaModel = new OpenAIChatCompletionsModel(
     ollamaClient, // 1st: The OpenAI client instance
-    'llama3.2:latest'    // 2nd: The model name string
+    'qwen2.5:1.5b'    // 2nd: The model name string
 );
 
+const firstAgent = new Agent({
+    model: ollamaModel,
+    name: "Agent",
+    instructions: "You are very Helpful agent that response and resolve the user queries"
+})
+
+async function main() {
+    const result = await run(firstAgent, "Which is best Rust vs Go ?  which one to choose?");
+    console.log(result.finalOutput);
+}
+
+main();
