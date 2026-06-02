4a2466a1fd3599048d0331c04f298b3d0ce888d8
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-06-02 21:37:37 +0530

    feat: implement weather retrieval and email notification tools for the agent

--- 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/02. Tool-Calling-in-Agent/agent-tools/tools.ts
+++ 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/02. Tool-Calling-in-Agent/agent-tools/tools.ts
@@ -1,10 +1,14 @@
+import 'dotenv/config'
 import { Agent, run, setTracingDisabled, tool } from "@openai/agents";
 import { ollamaModel } from "../agent/ollama";
 import { z } from "zod";
 import axios from 'axios';
+import { sendEmail } from "../resend/resend.config";
 
 // This is will need if you are using ollama because 
 setTracingDisabled(true)
+
+const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS as string;
 
 const getWeatherAgent = tool({
     name: 'get_Weather',
@@ -16,7 +20,7 @@ const getWeatherAgent = tool({
     }),
     execute: async ({ city }: { city: string }) => {
         const url = `https://wttr.in/${encodeURIComponent(city.toLowerCase())}?format=3`;
-        const res = await axios.post(url, {
+        const res = await axios.get(url, {
             headers: {
                 Accept: 'text/plain'
             }
@@ -42,40 +46,37 @@ const sendEmailAgent = tool({
         subject: z.string(),
         body: z.string(),
     }),
+
     execute: async ({ to, subject, body }: { to: string, subject: string, body: string }) => {
         console.log('Tool Calling 🤖🤖🤖🤖🤖🤖🤖🤖🤖🤖🤖🤖🤖')
-        console.log('to', to);
-        console.log('subject', subject);
-        console.log('body', body);
+        await sendEmail(EMAIL_ADDRESS, subject, body);
         return `
-            The email has been sent to ${to}
+            The email has been sent to ${EMAIL_ADDRESS}
         `
     }
 })
 
 
-const WeatherAgent = new Agent({
-    name: 'Weather Agent',
+const WeatherAgentData = new Agent({
+    name: 'Weather Agent Send Data to Email',
     model: ollamaModel,
     instructions: `
         You are weather Agent and you are the best at predicting the weather.
         You can also predict the weather for the future.
         Use the tools to get the weather for the future.
+        and send Weather data to user's email address using send email tool after all process.
     `,
-    tools: [getWeatherAgent]
+    tools: [getWeatherAgent, sendEmailAgent]
 })
 
 async function main(query: string) {
-    const result = await run(WeatherAgent, query);
+    const result = await run(WeatherAgentData, query);
     console.log(result.finalOutput);
 }
 
-main(`New York,
-     San Francisco, 
-     Dubai, 
-     Kyoto, 
-     Tokyo, 
-     Texas weathers of now, ( 
+main(`New York, weathers of now, ( 
     give all cities weather in bullet form
     add emojis of weathers accordingly
-    )`);
+    and send all weather to ${EMAIL_ADDRESS} using send email tool after all process.
+    send all data to my email address only.
+    )`)    
