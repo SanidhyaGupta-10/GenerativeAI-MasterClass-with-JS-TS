074f9a088349fa95a494b957d08b306ac3a34b21
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-06-02 20:45:49 +0530

    feat: implement weather tool and agent using @openai/agents and axios

--- 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/02. Tool-Calling-in-Agent/agent-tools/tools.ts
+++ 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/02. Tool-Calling-in-Agent/agent-tools/tools.ts
@@ -1,8 +1,33 @@
-import { Agent, run, setTracingDisabled } from "@openai/agents";
+import { Agent, run, setTracingDisabled, tool } from "@openai/agents";
 import { ollamaModel } from "../agent/ollama";
+import { z } from "zod";
+import axios from 'axios';
 
 // This is will need if you are using ollama because 
 setTracingDisabled(true)
+
+const getWeatherAgent = tool({
+    name: 'get_Weather',
+    description: `
+        returns the curret weather information for thr given city 
+    `,
+    parameters: z.object({
+        city: z.string().describe("Enter city name")
+    }),
+    execute: async ({ city }: { city: string }) => {
+        const url = `https://wttr.in/${encodeURIComponent(city.toLowerCase())}?format=3`;
+        const res = await axios.post(url, {
+            headers: {
+                Accept: 'text/plain'
+            }
+        });
+        const result = res.data;
+        return `
+            The current weather in ${city} is ${result}
+        `
+    }
+})
+
 
 const WeatherAgent = new Agent({
     name: 'Weather Agent',
@@ -11,7 +36,8 @@ const WeatherAgent = new Agent({
         You are weather Agent and you are the best at predicting the weather.
         You can also predict the weather for the future.
         Use the tools to get the weather for the future.
-    `
+    `,
+    tools: [getWeatherAgent]
 })
 
 async function main(query: string) {
@@ -19,4 +45,4 @@ async function main(query: string) {
     console.log(result.finalOutput);
 }
 
-main('Varanasi Weather');
+main('Varanasi Weather, call');
