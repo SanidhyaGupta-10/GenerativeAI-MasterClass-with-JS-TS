dd07d8014bfd26ff87d7d058d014a4d4776c488f
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-06-11 21:20:09 +0530

    Context-ManageMent of RuntimeLocal Agent

--- 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/10. RuntimeLocal-ContextManagement/index.ts
+++ 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/10. RuntimeLocal-ContextManagement/index.ts
@@ -1,17 +1,35 @@
-import { run, Agent } from '@openai/agents';
+import { run, Agent, tool, RunContext } from '@openai/agents';
 import { groqModel } from './agent/openai';
-
+import z from 'zod'
 
 interface MyContext {
     userId: string,
     userName: string
+
+    fetchUserInfoFromDB: () => Promise<string>
 }
+
+const getUserInfo = tool({
+    name: 'get_user_info',
+    description: 'Get the user info',
+    parameters: z.object({
+        // required empty object is mandatory if query is optional
+        query: z.string().optional().describe('Optional filter keyword for plans'),
+    }),
+ 
+    execute: async (_, ctx?: RunContext<MyContext>): Promise<string | undefined> => {
+        return await ctx?.context.fetchUserInfoFromDB()
+        // return `Userid=${ctx?.context.userId}, \n 
+        //         UserName=${ctx?.context.userName}`
+    }
+})
 
 const CustomerQueryAgent = new Agent<MyContext>({
     name: `Customer_Query_Resolver`,
     model: groqModel,
+    tools: [getUserInfo],
     instructions: ({ context }) => {
-        return `You are an expert agent in resolving the user's query ${context}`
+        return `You are an expert agent in resolving the user's query`
     }
 });
 
@@ -23,7 +41,8 @@ async function main(query: string, ctx: MyContext) {
     console.log(`Response: ${response.finalOutput}`)
 };
 
-main(`Hello, i can't log in`, {
+main(`Hey what is my name?`, {
     userId: "1",
-    userName: "Sanidhya"
+    userName: "Sanidhya",
+    fetchUserInfoFromDB: async () => "UserId=1, UserName=Sanidhya"
 })
