cf1805a39247698fac081c506fa6321d03ca74a3
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-06-16 21:15:31 +0530

    feat: implement streamable MCP server integration for GitHub documentation agent

---
+++ 2. Advance-GenAI/12. OpenAI-Agent-SDK-Series/13. MCP-ModelContextProtocol/streamableMCP.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import { Agent, run, MCPServerStreamableHttp } from '@openai/agents';
+
+const githubMcpSever = new MCPServerStreamableHttp({
+  url: 'https://gitmcp.io/openai/codex',
+  name: 'GitMCP Documentation Server',
+});
+
+const agent = new Agent({
+  name: 'MCP Assistant',
+  instructions: 'You must always use the MCP tools to answer questions.',
+  mcpServers: [githubMcpSever],
+});
+
+async function main(q: string) {
+  await githubMcpSever.connect();
+  const result = await run(agent, q);
+  console.log(result.finalOutput);
+  await githubMcpSever.close();
+}
+
+main('What is this repo about?');
